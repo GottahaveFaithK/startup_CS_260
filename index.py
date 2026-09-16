44110332ea3efd80a2cceed77b06180cd6f6e78d
Faith Kirkham <faithkirkham12@gmail.com>
2026-09-16 13:12:26 -0600

    general thought outline

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 #placeholder
+#Header is the navigation page
+
+#Content: Do I want to have a login page before you can play or view the game, or
+# do I want it to open to play but leaderboard requires a score?
+# if seperate login page, this is the login page. Otherwise this might be login and gameplay,
+# login would be displayed in header... would need to switch to logout. Is that more complex? Less?
+# Other pages needed: About, Leaderboard
+
+#Footer must contain link to github repository
